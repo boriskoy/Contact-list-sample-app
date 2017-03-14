c3cbf6ed3aec68a1bbe00231d52e63de0bbcba60
shoaib <shoaib.rafi@virtual-force.com>
2017-03-14 15:31:42 +0500

    contact remove and update request added

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -19,5 +19,34 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
 	  });
   }
+
+  $scope.removeContact = function(id){
+  	$http.delete('/contactlist/'+id).then(function (success){
+	    	refresh();
+	    },function (error){
+
+	  });
+  }
+
+  $scope.editContact = function(id){
+    $http.get('/contactlist/'+id).then(function (success){
+      $scope.contact = success.data;
+      },function (error){
+
+    });
+  }
+
+  $scope.updateContact = function(){
+  	console.log($scope.contact._id);
+    $http.put('/contactlist/'+ $scope.contact._id, $scope.contact).then(function (success){
+      console.log(success.data);
+      refresh();
+      },function (error){
+    });
+  }
+
+  $scope.clear = function(){
+    $scope.contact = {};
+  }
     
 }]);
