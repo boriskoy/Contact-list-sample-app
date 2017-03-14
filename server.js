811e8c05c7b3c2d9ab977003545e2d4af1993d97
shoaib <shoaib.rafi@virtual-force.com>
2017-03-14 12:02:58 +0500

    added mongojs and connected db to app

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist'])
 
-app.get('/contactlist', function(req, res){
+app.get('/contactlist', function (req, res){
 	console.log("i received a request");
-	var contactList = [
-    	{
-    		name: "shoaib",
-    		email: "shoaib@gmail.com",
-    		number: "111 111 1111"
-    	},
-    	{
-    		name: "jojo",
-    		email: "jojo@gmail.com",
-    		number: "222 222 2222"
-    	},
-    	{
-    		name: "don",
-    		email: "don@gmail.com",
-    		number: "333 333 3333"
-    	}
-    ];
-    res.json(contactList);
+	db.contactlist.find(function (err, docs) {
+    res.json(docs);
+  });
 });
 
 app.use(express.static(__dirname + "/public"));
