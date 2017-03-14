5b2ffc1d43ef61bfcacca51d3804f105717e34c9
shoaib <shoaib.rafi@virtual-force.com>
2017-03-14 12:37:38 +0500

    save data from form into db

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,11 +1,23 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
 
-    $http.get("/contactlist").then(function (success){
-    	$scope.contactList = success.data;
-    },function (error){
+  var refresh = function(){
+  	$http.get("/contactlist").then(function (success){
+	    	$scope.contactList = success.data;
+	    	$scope.contact = {};
+	    },function (error){
 
-   });
+	  });
+  }
+
+  refresh();
+
+  $scope.addContact = function(){
+  	$http.post("/contactlist", $scope.contact).then(function (success){
+	    	refresh();
+	    },function (error){
+
+	  });
+  }
     
 }]);
